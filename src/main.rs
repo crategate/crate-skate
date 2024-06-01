c383f15d4881d188f95604c94b0ac99b2fa829f6
crategate <sevensidedsilver@gmail.com>
2024-06-01 15:01:43 -0400

    trying multiple buttons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use crate::glib::clone;
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow, Button};
 use soloud::*;
+use std::cell::Cell;
+use std::rc::Rc;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
@@ -16,36 +19,53 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+    let number = Rc::new(Cell::new(0));
     // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
+    let button_increase = Button::builder()
+        .label("increase")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    // Connect to "clicked" signal of `button`
-    //tokentest
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
+    let button_decrease = Button::builder()
+        .label("decrease")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
 
-        let sl = Soloud::default().unwrap();
-        let mut wav = audio::Wav::default();
-        wav.load_mem(include_bytes!("../assets/Laid-Off.mp3"))
-            .unwrap();
-        sl.play(&wav);
-        while sl.voice_count() > 0 {
-            std::thread::sleep(std::time::Duration::from_millis(100));
-        }
-    });
+    // Connect callbacks
+    // When a button is clicked, `number` should be changed
+    button_increase.connect_clicked(clone!(@strong number => move |_| {
+        number.set(number.get() + 1);
+    }));
+
+    button_decrease.connect_clicked(move |_| number.set(number.get() - 1));
+
+    // Connect to "clicked" signal of `button`
+    // button.connect_clicked(|button| {
+    //     // Set the label to "Hello World!" after the button has been clicked on
+    //     button.set_label("Hello World!");
+
+    //     let sl = Soloud::default().unwrap();
+    //     let mut wav = audio::Wav::default();
+    //     wav.load_mem(include_bytes!("../assets/Laid-Off.mp3"))
+    //         .unwrap();
+    //     sl.play(&wav);
+    //     while sl.voice_count() > 0 {
+    //         std::thread::sleep(std::time::Duration::from_millis(100));
+    //     }
+    // });
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
+        .child(&button_increase)
+        .child(&button_decrease)
         .build();
 
     // Present window
